dbf399dbb765fd936ca76b318d207044943b4658
Hector Franco T <tor6.franco6@hotmail.com>
2020-05-20 22:00:34 -0500

    Pipelines

--- generator/main.py
+++ generator/main.py
@@ -1,4 +1,4 @@
-from templates import generate_template, custom_render_template
+from pipelines import global_pipeline
 
 if __name__ == '__main__':
-    print(custom_render_template(generate_template('test'), a_string='Jjaja', a_number=1))
+    global_pipeline()
--- generator/templates/__init__.py
+++ generator/templates/__init__.py
@@ -4,9 +4,15 @@ import os
 
 def generate_template(template_name):
     template_dir = os.path.join(get_config()['templates_path'], f'{template_name}.html')
-    print(template_dir)
     with open(template_dir, mode='r') as f:
         return Template(f.read())
 
 def custom_render_template(template: Template, *args, **kwargs):
     return template.render(*args, **kwargs)
+
+def write_rendered_template_to_file(template: Template, filename: str, file_path = '', *args, **kwargs):
+    file_dir = os.path.join(get_config()['content_path'], file_path, f'{filename}.html')
+    if not os.path.exists(os.path.dirname(file_dir)):
+        os.makedirs(os.path.dirname(file_dir))
+    with open(file_dir, mode='w') as f:
+        f.write(custom_render_template(template, *args, **kwargs))
